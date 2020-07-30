5aa982aa23b0970cf68df0a62d6fe8f1f4f981fc
Muhammad Orabi <orabi.dev@gmail.com>
2020-07-30 15:18:27 +0200

    fix ews flow

--- exchange.go
+++ exchange.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"log"
 	"os"
-	"time"
 
+	"github.com/MuhammadOrabi/ews"
 	"github.com/gin-gonic/gin"
-	"github.com/mhewedy/ews"
 )
 
 func getUserAvailability(c *gin.Context) {
 	username := os.Getenv("EWS_USERNAME")
 	password := os.Getenv("EWS_PASSWORD")
-	url := os.Getenv("EWS_URL")
-	// hostname := os.Getenv("EWS_HOSTNAME")
 
-	client := ews.NewClient(url, username, password, &ews.Config{
-		Dump: true, NTLM: true,
-	})
-	startTime, _ := time.Parse("2006-01-02", "2020-07-16")
-	endTime, _ := time.Parse("2020-08-16", "2020-07-16")
+	hostname := os.Getenv("EWS_HOSTNAME")
+	if dn := hostname; len(dn) > 0 {
+		username = dn + "\\" + username
+	}
+
+	client := ews.NewClient(
+		os.Getenv("EWS_URL"),
+		username,
+		password,
+		&ews.Config{
+			Dump:    false,
+			NTLM:    true,
+			SkipTLS: false,
+		},
+	)
+
 	r := &ews.GetUserAvailabilityRequest{
 		TimeZone: ews.TimeZone{
 			Bias: 480,
@@ -51,35 +58,21 @@ func getUserAvailability(c *gin.Context) {
 		},
 		FreeBusyViewOptions: ews.FreeBusyViewOptions{
 			TimeWindow: ews.TimeWindow{
-				StartTime: startTime,
-				EndTime:   endTime,
+				StartTime: "2020-08-02T00:00:00Z",
+				EndTime:   "2020-08-02T01:00:00Z",
 			},
 			MergedFreeBusyIntervalInMinutes: 60,
 			RequestedView:                   "Detailed",
 		},
 	}
 
-	// xmlBytes, err := xml.MarshalIndent(r, "", "  ")
-	// if err != nil {
-	// 	log.Println("err", err)
-	// 	c.JSON(400, err)
-	// 	return
-	// }
-
 	resp, err := ews.GetUserAvailability(client, r)
 	if err != nil {
-		log.Println("Error on unmarshaling xml. ", err.Error())
 		c.JSON(400, gin.H{
-			"error": err.Error(),
+			"error": err,
 		})
 		return
 	}
-	// bb, err := client.SendAndReceive(xmlBytes)
-	// log.Println("bb, err", bb, err)
-	// if err != nil {
-	// 	c.JSON(400, err)
-	// 	return
-	// }
 
 	c.JSON(200, gin.H{
 		"result": resp,
